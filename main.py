dbb903a63c5161124b0aaa59ca5a6bec39cb332b
tmdgusya <dev0jsh@gmail.com>
2025-04-17 00:08:28 +0900

    exception handler

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+
 from user.interface.controllers.user_controller import router as user_routers
 
 app = FastAPI()
 app.include_router(user_routers)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError
+):
+    return JSONResponse(
+        status_code=400,
+        content=exc.errors(),
+    )
 
 @app.get("/")
 def hello():
